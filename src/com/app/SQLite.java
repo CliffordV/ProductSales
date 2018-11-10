4bf9f0b964203f48b0e6c048f1d9020412ea9774
CliffordV <vanderclifford1@gmail.com>
2018-11-11 02:38:06 +0800

    Update src/com/app/SQLite.java

--- src/com/app/SQLite.java
+++ src/com/app/SQLite.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.app;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 
-/**
- *
- * @author ITE Account
- */
 public class SQLite {
     
     static java.sql.Connection conn  = null;
