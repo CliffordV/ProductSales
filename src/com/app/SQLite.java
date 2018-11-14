fc6b3dbc675e493c3a1384085671543009c451ee
CliffordV <vanderclifford1@gmail.com>
2018-11-14 14:35:06 +0800

    Update src/com/app/SQLite.java

--- src/com/app/SQLite.java
+++ src/com/app/SQLite.java
@@ -7,8 +7,8 @@ public class SQLite {
     
     static java.sql.Connection conn  = null;
     static java.sql.Statement stmt = null;
-    static String url = "jdbc:sqlite:C:\\Users\\VanderClifford\\Documents\\NetBeansProjects\\ProductSales\\src\\com\\app\\sqlite.sqlite";
-//    static String url = "<PATH>";
+//    static String url = "jdbc:sqlite:C:\\Users\\VanderClifford\\Documents\\NetBeansProjects\\ProductSales\\src\\com\\app\\sqlite.sqlite";
+    static String url = "<PATH>";
     static String error = "";
     
     public static boolean openDB(){
